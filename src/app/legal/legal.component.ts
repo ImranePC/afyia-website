fcee2506f0dbfa35e58d130aeabbda46d0e9f5e8
Imrane MARBOUH <imrane.marbouh@innovative-diagnostics.com>
2024-09-03 17:13:19 +0200

    legal mentions page

--- src/app/legal/legal.component.ts
+++ src/app/legal/legal.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-legal',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent, TranslateModule],
   templateUrl: './legal.component.html',
   styleUrl: './legal.component.scss'
 })
