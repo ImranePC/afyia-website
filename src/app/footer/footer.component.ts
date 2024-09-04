f3b3c0edd47f9274eac13e65597a2d9478a7153a
Imrane MARBOUH <imrane.marbouh@innovative-diagnostics.com>
2024-09-04 09:53:09 +0200

    footer fix

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [RouterLink, TranslateModule],
+  imports: [RouterLink, TranslateModule, FontAwesomeModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  faUpRightFromSquare = faUpRightFromSquare;
 }
