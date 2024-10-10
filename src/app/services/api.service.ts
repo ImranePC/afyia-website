580da78ce8d603b8ba908631f4d17c3f76d9846d
Imrane MARBOUH <imrane.marbouh@innovative-diagnostics.com>
2024-10-10 14:20:38 +0200

    added class ApiService

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const API_URL = 'http://localhost:3001';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: HttpClient) { }
+
+  sendMessage(data: any): Observable<any> {
+    console.log(data);
+
+    return this.http.post(`${API_URL}/send-message`, data);
+  }
+}
