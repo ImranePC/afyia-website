987065ef88f83bb33e9d63559277b4d2396c3d90
Imrane MARBOUH <imrane.marbouh@innovative-diagnostics.com>
2024-11-05 11:32:31 +0100

    custom select menu

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -2,13 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { ParallaxDirective } from '../directives/parallax.directive';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { ArianeComponent } from '../ariane/ariane.component';
-import { faBoxOpen } from '@fortawesome/free-solid-svg-icons';
+import { faBoxOpen, faCaretDown } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FormsModule } from '@angular/forms';
 import { ProductService } from '../services/product.service';
 import { ProductCardComponent } from './product-card/product-card.component';
 import { RouterModule } from '@angular/router';
 import { AppService } from '../services/app.service';
+import { CommonModule } from '@angular/common';
+import { ClickOutsideDirective } from '../directives/click-outside.directive';
 
 export const PATHOGENS = [
   { label: 'Monkeypox virus', value: 'mpox' },
@@ -35,6 +37,8 @@ export const TECHNOLOGIES = [
     FormsModule,
     ProductCardComponent,
     RouterModule,
+    CommonModule,
+    ClickOutsideDirective,
   ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
@@ -43,6 +47,8 @@ export class ProductsComponent implements OnInit {
   products: any;
 
   faBoxOpen = faBoxOpen;
+
+  faCaretDown = faCaretDown;
 
   pathogenList = PATHOGENS;
 
@@ -80,6 +86,24 @@ export class ProductsComponent implements OnInit {
     this.selectedFilter.technology = null;
   }
 
+  selectPathogen(pathogen: any): void {
+    this.selectedFilter.pathogen = pathogen;
+  }
+
+  selectTechnology(technology: any): void {
+    this.selectedFilter.technology = technology;
+  }
+
+  openDropdown(dropdown: HTMLElement, event: any): void {
+    event.stopPropagation();
+    dropdown.style.display = 'block';
+  }
+
+  closeDropdown(dropdown: HTMLElement, event: any): void {
+    event.stopPropagation();
+    dropdown.style.display = 'none';
+  }
+
   get hasFilter(): boolean {
     return Object.values(this.selectedFilter).some((value) => value !== null);
   }
@@ -90,8 +114,8 @@ export class ProductsComponent implements OnInit {
     }
 
     return this.products.filter((product: any) =>
-      (this.selectedFilter.technology ? product.technology.includes(this.selectedFilter.technology) : true)
-      && (this.selectedFilter.pathogen ? product.pathogens.includes(this.selectedFilter.pathogen) : true)
+      (this.selectedFilter.technology ? product.technology.includes(this.selectedFilter.technology.value) : true)
+      && (this.selectedFilter.pathogen ? product.pathogens.includes(this.selectedFilter.pathogen.value) : true)
     );
   }
 }
