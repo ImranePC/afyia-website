d41703c15c384e8c2be65b95a3ecc6864895e291
Imrane MARBOUH <imrane.marbouh@innovative-diagnostics.com>
2025-02-07 15:29:35 +0100

    handle ssl in production

--- api/server.js
+++ api/server.js
@@ -1,14 +1,17 @@
 require('dotenv').config({ path: './api/.env' });
-
 const express = require('express');
+const fs = require('fs');
+const https = require('https');
 const cors = require('cors');
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./api/afyiadb.sqlite');
+const rateLimit = require('express-rate-limit');
+
 const app = express();
+const db = new sqlite3.Database('./api/afyiadb.sqlite');
 const PORT = process.env.PORT;
 const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
-const rateLimit = require('express-rate-limit');
 const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 100 });
+const ENV = process.env.NODE_ENV;
 
 const corsOptions = {
   origin: (origin, callback) => {
@@ -21,6 +24,7 @@ const corsOptions = {
     }
   }
 };
+
 
 app.use(express.json());
 app.use(cors());
@@ -62,9 +66,22 @@ app.post('/send-message', (req, res) => {
   });
 });
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log('API is listening');
-});
+if (ENV === 'production') {
+  const privateKey  = fs.readFileSync(process.env.SSL_KEY, 'utf8');
+  const certificate = fs.readFileSync(process.env.SSL_CERT, 'utf8');
+  const credentials = {
+    key: privateKey,
+    cert: certificate,
+  };
+
+  https.createServer(credentials, app).listen(PORT, () => {
+    console.log('HTTPS: Server listening on port ' + PORT);
+  });
+} else {
+  app.listen(PORT, () => {
+    console.log('HTTP : Server listening on port ' + PORT);
+  });
+}
 
 function saveMessage(res, data) {
   console.log(data);
