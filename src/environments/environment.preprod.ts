1de971a1cae0a3abea6dfaaef9d1addb05d1a466
Imrane MARBOUH <imrane.marbouh@innovative-diagnostics.com>
2026-04-08 14:07:15 +0200

    environments variable and database snapshot

--- src/environments/environment.preprod.ts
+++ src/environments/environment.preprod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'http://192.168.97.60:3001',
+  imageUrl: 'http://192.168.97.60:3001/assets'
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://afyia-diagnostics.com:3001',
+  imageUrl: 'https://afyia-diagnostics.com:3001/assets'
 }
