34121dab62a3ac44719c9ec22af1a00b331275a0
Imrane MARBOUH <imrane.marbouh@innovative-diagnostics.com>
2026-09-11 14:27:16 +0200

    fix: fix health layout and content, removed reveal class for data-scroll (locomotive)

--- src/app/views/approach/health/health.component.ts
+++ src/app/views/approach/health/health.component.ts
@@ -1,46 +1,42 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
-import { AppService } from '../../../services/app.service';
 import { BannerComponent } from '../../../components/banner/banner.component';
 import { CarouselComponent, CarouselElement } from '../../../components/carousel/carousel.component';
+import { RouterModule } from '@angular/router';
+import { LocalizedLinkPipe } from '../../../pipes/localized-link.pipe';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-health',
   standalone: true,
-  imports: [BannerComponent, CarouselComponent, TranslateModule],
+  imports: [BannerComponent, CarouselComponent, TranslateModule, RouterModule, LocalizedLinkPipe, FontAwesomeModule],
   templateUrl: './health.component.html',
   styleUrl: './health.component.scss'
 })
 export class HealthComponent {
+  faArrowRight = faArrowRight;
 
-    links: CarouselElement[] = [
-      {
-        title: 'header.technology',
-        image: 'assets/img/banner/banner_software.webp',
-        route: 'software',
-      },
-      {
-        title: 'header.biotechnology',
-        image: 'assets/img/team/img_margot.jpg',
-        route: 'biotechnology',
-      },
-      {
-        title: 'header.process',
-        image: 'assets/img/img_process_2.jpg',
-        route: 'industrial-process',
-      },
-      {
-        title: 'header.news',
-        image: 'assets/img/banner/banner_news_alt.webp',
-        route: 'news',
-      }
-    ];
-
-  constructor(
-    private appService: AppService,
-  ) { }
-
-  ngOnInit(): void {
-    this.appService.initScrollReveal();
-  }
+  links: CarouselElement[] = [
+    {
+      title: 'header.technology',
+      image: 'assets/img/banner/banner_software.webp',
+      route: 'software',
+    },
+    {
+      title: 'header.biotechnology',
+      image: 'assets/img/team/img_margot.jpg',
+      route: 'biotechnology',
+    },
+    {
+      title: 'header.process',
+      image: 'assets/img/img_process_2.jpg',
+      route: 'industrial-process',
+    },
+    {
+      title: 'header.news',
+      image: 'assets/img/banner/banner_news_alt.webp',
+      route: 'news',
+    }
+  ];
 }
